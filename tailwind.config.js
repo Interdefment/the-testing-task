58dc951ff76ac5ce113525f44eadd96ad8be78fe
Interdefment <30928257+Interdefment@users.noreply.github.com>
2022-02-24 13:09:21 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         './src/**/*.scss',
         './node_modules/@vechaiui/**/*.{js,ts,jsx,tsx}',
     ],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: 'class',
     theme: {
         extend: {
             colors: {
